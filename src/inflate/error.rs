e70bd20ddcf9d76712524c360925226e2d46d4d4
Honza Špaček <patek.mail@gmail.com>
2013-06-17 15:17:57 +0200

    Add failing tests for fixed blocks

--- src/inflate/error.rs
+++ src/inflate/error.rs
@@ -3,7 +3,8 @@ use std::num::{ToStrRadix};
 #[deriving(Clone,Eq)]
 pub enum Error {
   BadBlockType(u8),
-  VerbatimLengthMismatch(u16,u16),
+  VerbatimLengthMismatch(u16, u16),
+  DistanceTooLong(uint, uint),
 }
 
 impl ToStr for Error {
@@ -11,10 +12,12 @@ impl ToStr for Error {
     match *self {
       BadBlockType(btype) =>
         fmt!("Bad block type %s", btype.to_str()),
-      VerbatimLengthMismatch(len,nlen) =>
+      VerbatimLengthMismatch(len, nlen) =>
         fmt!("Mismatch between verbatim block length %016s (%s) \
               and its inverse %016s (%s)",
           len.to_str_radix(2), len.to_str(), nlen.to_str_radix(2), nlen.to_str()),
+      DistanceTooLong(pos, dist) =>
+        fmt!("Distance %u on position %u is too long", dist, pos),
     }
   }
 }
--- src/inflate/fixed.rs
+++ src/inflate/fixed.rs
@@ -107,3 +107,73 @@ pub static fixed_table: [(u16,uint), ..32] = [
   (128,3), (136,3), (144,4), (160,4), (176,4), (192,4), (208,4), (224,4),
   (240,4), (256,2), (260,2), (264,2), (268,2), (272,2), (276,2), (280,3),
 ];
+
+#[cfg(test)]
+mod test {
+  use inflate::test_helpers::*;
+  use std::uint;
+
+  #[test]
+  fn test_inflate_fixed() {
+    // literals only
+    assert_eq!(inflate_ok(&[
+        0b11100011, 0b00010010, 0b10010001, 0b00000011, 0b00000000
+      ]),
+      ~[10, 20, 30]
+    );
+
+    // longer data
+    assert_eq!(inflate_ok(&[
+        0b11111011, 0b10110001, 0b01101010, 0b01101001,
+        0b11111010, 0b10110111, 0b01111111, 0b01011110,
+        0b11001101, 0b10111011, 0b10101011, 0b00000001
+      ]),
+      ~[248, 170, 165, 103, 246, 254, 74, 131, 187, 123]
+    );
+
+    // simple length-distance pair
+    assert_eq!(inflate_ok(&[
+        0b10010011, 0b11010011, 0b00000010,
+        0b00000010, 0b00001101, 0b00000000
+      ]),
+      ~[30, 42, 42, 42, 42, 40]
+    );
+
+    /* a longer length-distance pair */
+    assert_eq!(inflate_ok(&[
+        0b11100011, 0b00010010, 0b01010011,
+        0b11000100, 0b00001101, 0b10111001,
+        0b11000100, 0b00010100, 0b00000001
+      ]),
+      ~[ 10, 22, 33, 22, 33, 22, 33, 22
+       , 33, 22, 33, 22, 33, 22, 33, 22
+       , 33, 22, 33, 22, 33, 22, 33, 22
+       , 33, 22, 33, 22, 33, 10, 22, 33]
+    );
+
+    /* long repetition and then long distance */
+    let res = inflate_ok(&[
+        0b00010011, 0b10010001, 0b11010011, 0b00110000, 0b10110010,
+        0b11100001, 0b00011010, 0b00000101, 0b00100011, 0b00001110,
+        0b10001000, 0b10000000, 0b00100010, 0b00011110, 0b00000000
+      ]);
+
+    assert_eq!(res.slice(0, 5), &[20,30,40,50,60]);
+    for uint::range(5, 505) |i| {
+      assert_eq!(res[i], 10);
+    }
+    assert_eq!(res.slice(505, 510), &[20,30,40,50,60]);
+  }
+
+  #[test]
+  fn test_inflate_fixed_errors() {
+    // the distance is too long (points before the start of input)
+    assert_eq!(inflate_err(&[
+        0b1110_0011, 0b0001_0010, 0b0000_0011,
+        0b0010_0010, 0b0000_0000
+      ]),
+      ~error::DistanceTooLong(2, 3)
+    );
+  }
+}
+
