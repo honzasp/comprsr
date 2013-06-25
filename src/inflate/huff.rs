a5a2fcd5bc2b54ae96eb7759967961cf60b1513f
Honza Špaček <patek.mail@gmail.com>
2013-06-25 13:28:20 +0200

    Implement the Huffman tree

--- src/inflate/huff.rs
+++ src/inflate/huff.rs
@@ -1,46 +1,99 @@
 use inflate::error;
+use std::iterator::*;
+use std::uint;
+use std::vec;
 
 pub struct Tree {
-  priv x: uint,
+  priv nodes: ~[u16],
 }
 
 struct Node(u16);
 
 impl Tree {
   pub fn new_empty() -> Tree {
-    fail!()
+    Tree { nodes: ~[] }
   }
 
-  pub fn new_from_lens(lens: &[u8]) -> Result<Tree, ~error::Error> {
-    fail!()
+  pub fn new_from_lens(bit_lens: &[u8]) -> Result<Tree, ~error::Error> {
+    let mut bl_count: ~[int] = ~[0];
+    let mut bl_syms: ~[~[u16]] = ~[~[]];
+    let mut max_bl: uint = 0;
+
+    for bit_lens.iter().enumerate().advance |(sym, &bl8)| {
+      let bl = bl8 as uint;
+      if bl > 0 {
+        if bl > max_bl {
+          vec::grow(&mut bl_count, bl - max_bl, &0);
+          vec::grow(&mut bl_syms, bl - max_bl, &~[]);
+          max_bl = bl;
+        }
+        bl_count[bl] += 1;
+        bl_syms[bl].push(sym as u16);
+      }
+    }
+
+    let mut nodes: ~[u16] = ~[0xffff];
+    let mut front: ~[u16] = ~[0];
+    let mut front_offset: uint = 0;
+
+    for uint::range(1, max_bl+1) |level| {
+      let mut new_front: ~[u16] = ~[];
+
+      for uint::range(front_offset, front.len()) |i| {
+        let front_n: u16 = front[i];
+        let zero_n: u16 = nodes.len() as u16;
+        let one_n: u16 = zero_n + 1;
+        nodes[front_n] = zero_n << 1;
+
+        vec::grow(&mut nodes, 2, &0xffff);
+        new_front.push(zero_n);
+        new_front.push(one_n);
+      }
+
+      front = new_front;
+      front_offset = 0;
+
+      if bl_syms[level].len() <= front.len() {
+        for bl_syms[level].each |&sym| {
+          let sym_n = front[front_offset];
+          nodes[sym_n] = (sym << 1) | 0b1;
+
+          front_offset = front_offset + 1;
+        }
+      } else {
+        return Err(~error::TooManyHuffCodesError(level));
+      }
+    }
+
+    Ok(Tree { nodes: nodes })
   }
 
   pub fn root(&self) -> Node {
-    fail!();
+    Node(0)
   }
 
-  pub fn zero_child(&self, n: Node) -> Node {
-    fail!();
+  pub fn zero_child(&self, Node(n): Node) -> Node {
+    Node(self.nodes[n] >> 1)
   }
 
-  pub fn one_child(&self, n: Node) -> Node {
-    fail!();
+  pub fn one_child(&self, Node(n): Node) -> Node {
+    Node((self.nodes[n] >> 1) + 1)
   }
 
-  pub fn is_leaf(&self, n: Node) -> bool {
-    fail!();
+  pub fn is_leaf(&self, Node(n): Node) -> bool {
+    self.nodes[n] & 0b1 != 0
   }
 
-  pub fn leaf_value(&self, n: Node) -> u16 {
-    fail!();
+  pub fn leaf_value(&self, Node(n): Node) -> u16 {
+    self.nodes[n] >> 1
   }
 
-  pub fn is_defined(&self, n: Node) -> bool {
-    fail!();
+  pub fn is_defined(&self, Node(n): Node) -> bool {
+    self.nodes[n] != 0xffff
   }
 }
 
-pub static undefined_leaf_value: u16 = 0;
+pub static undefined_leaf_value: u16 = 0xffff >> 1;
 
 #[cfg(test)]
 mod test {
